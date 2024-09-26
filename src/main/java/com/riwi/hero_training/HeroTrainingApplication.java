1d0914a140a32116ca349bc22212261fe774d18f
EmanuelOH <>
2024-09-26 15:25:25 -0500

    feat: comienzo

---
+++ src/main/java/com/riwi/hero_training/HeroTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.hero_training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeroTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riwi/hero_training/HeroTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.hero_training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
